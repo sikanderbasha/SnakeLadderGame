eb60e62e65a5fb46b4f5480d0da60b7e1eb2a8be
Sikander <sikku.rk405@gmail.com>
2022-01-30 21:12:47 +0530

    [Sikander] Added single player position program

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Game");
+            Singleplayer SinglePlay = new Singleplayer();
+            SinglePlay.StartPlay();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/Singleplayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Singleplayer
+    {
+        public int StartPoint = 0; 
+        public void StartPlay()
+        {
+            Console.WriteLine("Game Started!"); 
+
+            Console.WriteLine("You are at " + StartPoint);
+        }
+    }
+}
