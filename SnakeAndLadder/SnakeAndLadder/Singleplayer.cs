712209d9482dbe7b06167735deddb78a2da14f29
Sikander <sikku.rk405@gmail.com>
2022-01-31 20:53:07 +0530

    [Sikander] Added player winning program

--- SnakeAndLadder/SnakeAndLadder/Singleplayer.cs
+++ SnakeAndLadder/SnakeAndLadder/Singleplayer.cs
@@ -9,7 +9,7 @@ namespace SnakeAndLadder
     internal class Singleplayer
     {
         int StartPoint = 0;
-        const int Ladder = 1, Snake = 2, No_Play = 0;
+        const int Ladder = 1, Snake = 2, No_Play = 0, WinningPosition = 100;
         
         Random random = new Random();
         public int Dice()
@@ -19,26 +19,44 @@ namespace SnakeAndLadder
 
         public void StartPlay()
         {
-            int option = random.Next(0, 3);
-            Console.WriteLine("Player Role......");
-            switch (option)
+            while (StartPoint < WinningPosition)
             {
+
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+
+                    case Ladder:
+                        if (StartPoint + Dice() <= 100)
+                        {
+                            StartPoint += Dice();
+                            Console.WriteLine("Player moves ahead to " + StartPoint);
+                        }
+                        break;
+
+                    case Snake:
+                        if (StartPoint - Dice() >= 0)
+                        {
+                            StartPoint = StartPoint - Dice();
+                            Console.WriteLine("Player moves behind to " + StartPoint);
+                        }
+                        break;
+
+                    case No_Play:
+                        Console.WriteLine("Player is in the same position");
+                        break;
+
+                }
                 
-                case Ladder:
-                    StartPoint += Dice();
-                    Console.WriteLine("Player moves ahead by " + StartPoint);
-                    break;
-                case Snake:
-                    StartPoint -= Dice();
-                    Console.WriteLine("Player moves behind by " + StartPoint);
-                    break;
-                case No_Play:
-                    Console.WriteLine("Player is in the same position");
-                    break;
-                default:
-                    break;
-            }
+                if (StartPoint >= 100)
+                {
+                    
+                    Console.WriteLine("Player Won!");
+                }
+               
+             }
+         
+           }
 
         }
     }
-}
